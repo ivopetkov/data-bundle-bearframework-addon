75bca8beecfdfb49fb46ce363fe236e78d5db178
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 18:29:47 +0300

    Fix in class registration.

--- index.php
+++ index.php
@@ -7,16 +7,16 @@
  * Free to use under the MIT license.
  */
 
-use \BearFramework\App;
+use BearFramework\App;
 use IvoPetkov\BearFramework\Addons\DataBundle;
 
 $app = App::get();
 $context = $app->context->get(__FILE__);
 
 $context->classes
-        ->add('\IvoPetkov\BearFramework\Addons\DataBundle', 'src/DataBundle.php')
-        ->add('\IvoPetkov\BearFramework\Addons\DataBundle\AlreadyExistsException', 'src/DataBundle/AlreadyExistsException.php')
-        ->add('\IvoPetkov\BearFramework\Addons\DataBundle\NotFoundException', 'src/DataBundle/NotFoundException.php');
+        ->add('IvoPetkov\BearFramework\Addons\DataBundle', 'src/DataBundle.php')
+        ->add('IvoPetkov\BearFramework\Addons\DataBundle\AlreadyExistsException', 'src/DataBundle/AlreadyExistsException.php')
+        ->add('IvoPetkov\BearFramework\Addons\DataBundle\NotFoundException', 'src/DataBundle/NotFoundException.php');
 
 $app->shortcuts->add('dataBundle', function() {
     return new DataBundle();
