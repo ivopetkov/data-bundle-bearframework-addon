3ced169684bd0ff53fde76a3db01551b70f7460b
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 13:18:38 +0300

    Updated comments.

--- ide-helper.php
+++ ide-helper.php
@@ -3,7 +3,7 @@
 /*
  * Data bundle addon for Bear Framework
  * https://github.com/ivopetkov/data-bundle-bearframework-addon
- * Copyright (c) 2017 Ivo Petkov
+ * Copyright (c) Ivo Petkov
  * Free to use under the MIT license.
  */
 
